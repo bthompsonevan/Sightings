1914e26bbba7c474c686595a19c6515d880eec27
bthompsonevan <bthompson.evan@gmail.com>
2020-02-22 22:54:21 -0800

    added repository and irepository classes

---
+++ Sightings/Models/Irepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sightings.Models
+{
+    public class Irepository
+    {
+    }
+}
---
+++ Sightings/Models/Repository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sightings.Models
+{
+    public class Repository : Irepository
+    {
+        private Dictionary<int, Sighting> sightingItems;
+
+        public Repository()
+        {
+            sightingItems = new Dictionary<int, Sighting>();
+            new List<Sighting>
+            {
+                new Sighting {SightingLocation = "Springfield", SightingDate = DateTime.Parse("02/22/2020")},
+                new Sighting {SightingLocation = "Springfield", SightingDate = DateTime.Parse("02/22/2020")},
+                new Sighting {SightingLocation = "Springfield", SightingDate = DateTime.Parse("02/22/2020")},
+                new Sighting {SightingLocation = "Springfield", SightingDate = DateTime.Parse("02/22/2020")},
+                new Sighting {SightingLocation = "Springfield", SightingDate = DateTime.Parse("02/22/2020")},
+                new Sighting {SightingLocation = "Springfield", SightingDate = DateTime.Parse("02/22/2020")}
+
+            }
+        }
+
+
+    }
+}
