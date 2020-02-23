c30ef1580175cd5d02e06aa4596d0603dbe4c946
bthompsonevan <bthompson.evan@gmail.com>
2020-02-22 22:02:23 -0800

    Added sightings model to project

---
+++ Sightings/Models/Sighting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sightings.Models
+{
+    public class Sighting
+    {
+        public int SightingID { get; set; }
+
+        public string SightingLocation { get; set; }
+
+        public DateTime SightingDate { get; set; }
+    }
+}
