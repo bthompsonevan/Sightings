5f9c21c63d8cfe2afc5c54892965774f931677ff
bthompsonevan <bthompson.evan@gmail.com>
2020-02-23 15:48:37 -0800

    added Lab7-WebApi.md file to Docs folder

---
+++ Docs/Lab7-WebApi.md
@@ -0,0 +1,11 @@
+#Lab 7 Web Api Document
+
+**Features Implemented**
+
+ * API verbs in SightingsController.cs
+	* HttpPut
+	* HttpGet
+	* HttpPost
+	* HttpPatch
+	* HttpDelete
+	
---
+++ Sightings/Controllers/SightingController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Sightings.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sightings.Controllers
+{
+    [Route("api/[controller]")]
+    public class SightingController : Controller
+    {
+        private Irepository repository;
+
+        public SightingController(Irepository repo) => repository = repo;
+
+        [HttpGet]
+        public IEnumerable<Sighting> Get() => repository.Sightings;
+
+        [HttpGet("{id}")]
+        public Sighting Get(int id) => repository[id];
+
+        [HttpPost] 
+        public Sighting Post([FromBody] Sighting sig) => 
+            repository.AddSighting(new Sighting 
+                { SightingLocation = sig.SightingLocation, SightingDate = sig.SightingDate });
+
+        [HttpPut]
+        public Sighting Put([FromBody] Sighting sig) =>
+            repository.UpdateSighting(sig);
+
+        [HttpPatch("{id}")]
+        public StatusCodeResult Patch(int id,
+            [FromBody]JsonPatchDocument<Sighting> patch)
+        {
+            Sighting sig = Get(id);
+            if (sig != null)
+            {
+                patch.ApplyTo(sig);
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) => repository.DeleteSighting(id);
+
+
+    }
+}
